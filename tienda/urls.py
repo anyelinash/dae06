e3cc060816629874f0c277f2b9da35a73260f4db
anyelinash <anyelina.sosa@tecsup.edu.pe>
2023-10-24 18:50:00 -0500

    tarea lab08 terminado

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 app_name = 'tienda'
+
 urlpatterns = [
     path('', views.index, name='index'), 
-    path('producto/<int:producto_id>/', views.producto, name='producto'),
-    path('categorias/<int:categoria_id>/', views.listado_categoria, name='listado_categoria'),
+    #lab08
+    path('producto', views.ProductoView.as_view(), name='productos'),
+    path('producto/<int:producto_id>/', views.producto),
+    path('categorias/<int:categoria_id>/', views.ProductoDetailView.as_view()),
+    path('categoria',views.CategoriaView.as_view()),
+    path('productos/<int:producto_id>/',views.producto,name='producto'),
+    path('categoria/<int:categoria_id>/',views.ProductosxCategoriaView.as_view(),name='categoria'),
+    path('categorias/<int:categoria_id>/',views.categoria,name='categorias')
 ]
